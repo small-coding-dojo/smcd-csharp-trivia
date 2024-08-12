e2b21041edd36be5f53bd91b90f3fcb7f156a9e1
Niklas Rueber <nrueber@igus.net>
2024-08-12 18:10:12 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:C#/Trivia/Tests/GameTests.cs

--- C#/Trivia/Tests/GameTests.cs
+++ C#/Trivia/Tests/GameTests.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using Trivia;
 using Xunit;
 using NSubstitute;
+using NSubstitute.ReceivedExtensions;
 
 namespace Tests;
 
@@ -17,6 +19,38 @@ public class GameTests
         game.Roll(1);
         
         writer.Received().WriteLine($"{testPlayerName}'s new location is 1");
+    }
+
+    [Fact]
+    public void Some()
+    {
+        // Niklas would like to keep this test because we don't have an agreed dice size limitation
+        const string testPlayerName = "TestPlayer 1";
+        
+        var writer = Substitute.For<IWriter>();
+        var game = new Game(writer);
+        game.Add(testPlayerName);
+        game.Roll(13);
+        
+        writer.Received().WriteLine($"{testPlayerName}'s new location is 1");
+    }
+    
+    [Fact]
+    public void Some2()
+    {
+        const string testPlayerName = "TestPlayer 1";
+        
+        var writer = Substitute.For<IWriter>();
+        var game = new Game(writer);
+        game.Add(testPlayerName);
+        game.Roll(6);
+        game.Roll(6);
+        game.Roll(1);
+        
+        writer.Received().WriteLine($"{testPlayerName}'s new location is 6");
+        writer.Received().WriteLine($"{testPlayerName}'s new location is 0");
+        writer.Received().WriteLine($"{testPlayerName}'s new location is 1");
+        // TODO: Lets continue here with: ensure that the sequence of calls is right / the last call is "new location is 1".
     }
     
     
